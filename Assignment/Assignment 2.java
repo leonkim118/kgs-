b55b798a4f055582c95375819219fb1815a2e72d
leonkim118 <leonkim110807@gmail.com>
2024-09-11 10:35:36 +0700

    Create Assignment 2.java

---
+++ Assignment/Assignment 2.java
@@ -0,0 +1,14 @@
+public class My {
+    public static void main(String[] args) {
+        int a = 4;
+        double b = 5.6;
+        double c = 4.5;
+        String d = "Hello World";
+        String e = "My First Program";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
